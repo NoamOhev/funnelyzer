d6eea1dbe543190bbadc327e68adb084c6886acd
NoamOhev <145936434+NoamOhev@users.noreply.github.com>
2025-01-27 20:03:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // אם תרצה להוסיף CSS בעתיד
-import FunnelAnalyzer from './App'; // זה הקובץ המרכזי שלך
+import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <FunnelAnalyzer />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
